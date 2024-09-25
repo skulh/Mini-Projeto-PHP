ae191f3ce9e140a624d697a977036db8fda436e2
SkZ21 <haslansilva5@gmail.com>
2024-09-25 15:12:29 -0300

    criado menuphp

--- menu.php
+++ menu.php
@@ -1,7 +1,9 @@
 <nav>
+    
     <ul>
         <li><a href="?pg=conteudo">Inicio</a></li>
         <li><a href="?pg=quemsomos">Quem somos</a></li>
         <li><a href="?pg=faleconosco">Fale conosco</a></li>
     </ul>
+
 </nav>
